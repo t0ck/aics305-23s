1c15f5b799774c1ce75f0106017beb0718452791
t0ck <t0ck@korea.ac.kr>
2023-06-16 06:20:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 기계학습을 이용한 IoT Sensor 트래픽 플로우 분류
+해당 리포지토리는 2023년도 봄학기 기계학습개론 최종 프로젝트로 진행한 **기계학습을 이용한 IoT Sensor 트래픽 플로우 분류**에 대한 전처리된 데이터셋, 소스코드와 결과를 담고 있습니다.
+
+## 데이터셋에 대한 정보
+본 프로젝트에서 사용한 데이터셋은 2022년도 IEEE Access Volume: 10에 게시된 *Edge-IIoTset: A New Comprehensive Realistic Cyber Security Dataset of IoT and IIoT Applications for Centralized and Federated Learning* 논문에서 제공하는 **Edge-IIoTset-dataset.zip**을 이용하였으며, 해당 데이터셋은 [IEEE-Dataport](http://ieee-dataport.org/8939 "Edge-IIoTset-dataset.zip")에서 접근 가능합니다.
+
+전처리된 데이터셋은 리포지토리에 포함된 [dataset](https://github.com/t0ck/aics305-23s/tree/main/dataset "dataset")폴더에서 확인하실 수 있습니다.
+
+## 파일 정보
+파일의 정보는 다음과 같습니다.
+
+파일 이름|파일 설명
+---|---
+test_case_01.py|해당 소스코드는 Temperature_and_Humidity.csv와 Water_Level.csv를 읽어와서 전처리 과정을 거친 후 dataset 폴더에 dataset_test_case_01.csv를 생성하고 DecisionTree 학습을 한 후 정확도를 출력합니다.
+test_case_02.py|해당 소스코드는 Temperature_and_Humidity.csv와 Flame_Sensor.csv를 읽어와서 Digital_Output으로 묶고, Heart_Level.csv, Soil_Moisture.csv, Water_Level.csv, phValue.csv, Sound_Sensor.csv를 읽어와서 Analog_Output으로 묶는 전처리 과정을 거친 후 dataset 폴더에 dataset_test_case_02.csv를 생성하고 DecisionTree 학습을 한 후 정확도를 출력합니다.
+test_case_03.py|해당 소스코드는 Temperature_and_Humidity.csv와 Flame_Sensor.csv를 읽어와서 Digital_Output으로 묶고, Heart_Level.csv, Soil_Moisture.csv, Water_Level.csv, phValue.csv, Sound_Sensor.csv를 읽어와서 Analog_Output으로 묶는 전처리 과정을 거친 후 dataset 폴더에 dataset_test_case_03-Digital.csv, dataset_test_case_03-Analog.csv를 생성하고 DecisionTree 학습을 한 후 정확도를 출력합니다.
+test_case_04.py|해당 소스코드는 Temperature_and_Humidity.csv, Flame_Sensor.csv, Heart_Level.csv, Soil_Moisture.csv, Water_Level.csv, phValue.csv, Sound_Sensor.csv를 읽어와서 전처리 과정을 거친 후 dataset 폴더에 dataset_test_case_04.csv를 생성하고 DecisionTree 학습을 한 후 정확도를 출력합니다.
+DecisionTree.ipynb|해당 주피터 노트북은 dataset에 존재하는 test_case_01.csv, test_case_02.csv, test_case_03-Digital.csv, test_case_03-Analog.csv, test_case_04.csv를 읽어와서 각각의 DecisionTree 학습을 진행하고 학습된 결과를 출력해줍니다.
+RandomForest.ipynb|해당 주피터 노트북은 dataset에 존재하는 test_case_01.csv, test_case_02.csv, test_case_03-Digital.csv, test_case_03-Analog.csv, test_case_04.csv를 읽어와서 각각의 RandomForest 학습을 진행하고 학습된 결과를 출력해줍니다.
+SVM.ipynb|해당 주피터 노트북은 dataset에 존재하는 test_case_01.csv, test_case_02.csv, test_case_03-Digital.csv, test_case_03-Analog.csv, test_case_04.csv를 읽어와서 각각의 SVM 학습을 진행하고 학습된 결과를 출력해줍니다.
+XGBoost.ipynb|해당 주피터 노트북은 dataset에 존재하는 test_case_01.csv, test_case_02.csv, test_case_03-Digital.csv, test_case_03-Analog.csv, test_case_04.csv를 읽어와서 각각의 XGBoost 학습을 진행하고 학습된 결과를 출력해줍니다.
---
+++ test_case_01.py
@@ -0,0 +1,76 @@
+# <-- import library -->
+import pandas as pd
+import numpy as np
+import datetime
+from tqdm import tqdm
+import os
+
+# <-- Machine-learning library -->
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# <-- config -->
+_TODAY = datetime.datetime.today().date()
+dataset_dic = './dataset/'
+pre_dataset = './dataset_test_case_01'
+IoT_list = ['Temperature_and_Humidity', 'Water_Level']
+features = ['src.port', 'flow_duration', 'mqtt_duration', 'mqtt_connection_duration', 'mqtt_connection_ack_duration', 'mqtt_disconnection_duration', 'IoT_label']
+
+# <-- data pre-processing -->
+if os.path.isfile(dataset_dic + pre_dataset + '.csv'):
+    df = pd.read_csv(dataset_dic + pre_dataset + '.csv')
+else:
+    new_list = [] #Using it for making DataFrame
+    for IoT in tqdm(IoT_list, desc='Data Pre-Processing', position=0): #Progress bar - 1
+        csv_df = pd.read_csv(dataset_dic + IoT + '.csv', low_memory=False)
+        #Initializing variables
+        session_dict = {}
+        for index, row in tqdm(csv_df.iterrows(), total=len(csv_df), desc=('IoT : ' + IoT), position=1, leave=False): #Progress bar - 2
+            if row['tcp.connection.syn'] == 1: #Flow start point
+                #Record start
+                session_dict = { int(row['tcp.srcport']) : [ int(pd.to_timedelta(row['frame.time']).total_seconds() * 10**6), ] }
+            if row['mqtt.msgtype'] == 1: #MQTT Connection
+                #Record time
+                if int(row['tcp.srcport']) in session_dict:
+                    session_dict[int(row['tcp.srcport'])].append(int(pd.to_timedelta(row['frame.time']).total_seconds() * 10**6), )
+            if row['mqtt.msgtype'] == 2: #MQTT Connection Ack
+                #Record time
+                if int(row['tcp.dstport']) in session_dict:
+                    session_dict[int(row['tcp.dstport'])].append(int(pd.to_timedelta(row['frame.time']).total_seconds() * 10**6), )
+            if row['mqtt.msgtype'] == 14: #MQTT Disconnection
+                #Record time
+                if int(row['tcp.srcport']) in session_dict:
+                    session_dict[int(row['tcp.srcport'])].append(int(pd.to_timedelta(row['frame.time']).total_seconds() * 10**6), )
+            if row['tcp.connection.fin'] == 1 and row['mqtt.msgtype'] != 14:
+                #Record end
+                if int(int(row['tcp.dstport'])) in session_dict:
+                    if len(session_dict[int(row['tcp.dstport'])]) == 4:
+                        session_dict[int(row['tcp.dstport'])].append(int(pd.to_timedelta(row['frame.time']).total_seconds() * 10**6), )
+                        flow_duration = session_dict[int(row['tcp.dstport'])][4] - session_dict[int(row['tcp.dstport'])][0]
+                        mqtt_duration = session_dict[int(row['tcp.dstport'])][3] - session_dict[int(row['tcp.dstport'])][1]
+                        mqtt_connection_duration = session_dict[int(row['tcp.dstport'])][1] - session_dict[int(row['tcp.dstport'])][0]
+                        mqtt_connection_ack_duration = session_dict[int(row['tcp.dstport'])][2] - session_dict[int(row['tcp.dstport'])][1]
+                        mqtt_disconnection_duration = session_dict[int(row['tcp.dstport'])][3] - session_dict[int(row['tcp.dstport'])][2]
+                        new_list.append([int(row['tcp.dstport']), flow_duration, mqtt_duration, mqtt_connection_duration, mqtt_connection_ack_duration, mqtt_disconnection_duration, IoT])
+    df = pd.DataFrame(new_list, columns=features)
+    df.to_csv(dataset_dic + pre_dataset + '-' + str(_TODAY) + '.csv')
+    os.system("ln --symbolic --relative " + dataset_dic + pre_dataset + '-' + str(_TODAY) + '.csv ' + dataset_dic + pre_dataset + '.csv')
+        
+# <-- train set split -->
+train_features = features[1:-1] #Excepting src.port & label
+target = features[-1] #label
+
+X = df[train_features]
+y = df[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=43)
+
+# <-- test train : DecisionTree -->
+dtc = DecisionTreeClassifier()
+dtc.fit(X_train, y_train)
+
+y_pred = dtc.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
